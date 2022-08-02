cf9bdac9cc7c981fefddacb67c3d456331596e54
Daniel da Silva <daniel-lg@hotmail.com>
2022-08-01 22:25:13 -0300

    alteração pontual

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const server=express();
 server.listen (3000, console.log("Sevidor rodando na porta 3000..."));
 server.get('/',(req,res)=>res.send("Heloo minha tropa!"))
 server.get('/users',(rep,res)=>res.send("Pagina de usuários!!"))
+
+// Alteração pontual
